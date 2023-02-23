cf15a7dbda54574a61a72f60fea28541692da579
Gabriela Pestana <gabrielasap19@gmail.com>
2023-02-23 17:22:22 -0300

    searching for patuent data

--- aparecida-nutricao/js/principal.js
+++ aparecida-nutricao/js/principal.js
@@ -1,3 +1,14 @@
 const titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+const paciente = document.querySelector("#primeiro-paciente");
+
+const tdPeso = paciente.querySelector(".info-peso");
+const peso = tdPeso.textContent;
+
+const tdAltura = paciente.querySelector(".info-altura");
+const altura = tdAltura.textContent;
+
+const imc = peso / (altura * altura);
+
+console.log(imc)
