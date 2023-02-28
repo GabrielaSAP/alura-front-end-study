d7fecf74fa5d68d516dac4c62262f504378b2e2b
Gabriela Pestana <gabrielasap19@gmail.com>
2023-02-28 19:28:56 -0300

    styles with javascript

--- aparecida-nutricao/js/principal.js
+++ aparecida-nutricao/js/principal.js
@@ -14,18 +14,20 @@ for(let i = 0; i < pacientes.length; i++){
     
     const tdImc = paciente.querySelector(".info-imc");
     
-    const pesoValido = true;
-    const alturaValida = true;
+    let pesoValido = true;
+    let alturaValida = true;
     
     if(peso <= 0 || peso >= 1000){
         console.log("Peso inválido")
         pesoValido = false;
         tdImc.textContent = "Peso inválido!";
+        paciente.classList.add("paciente-invalido");
     }
     if(altura <= 0 || altura >= 3.00){
         console.log("Altura inválida");
         alturaValida = false;
         tdImc.textContent = "Altura inválida!";
+        paciente.classList.add("paciente-invalido");
     }
     
     if(alturaValida && pesoValido){
