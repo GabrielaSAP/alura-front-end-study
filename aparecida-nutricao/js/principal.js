74d9f0992cea30bb0d36f42629432488394f6d65
Gabriela Pestana <gabrielasap19@gmail.com>
2023-02-28 17:53:10 -0300

    replicating validation and imc calc for all patients

--- aparecida-nutricao/js/principal.js
+++ aparecida-nutricao/js/principal.js
@@ -1,31 +1,35 @@
 const titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-const paciente = document.querySelector("#primeiro-paciente");
+const pacientes = document.querySelectorAll(".paciente");
 
-const tdPeso = paciente.querySelector(".info-peso");
-const peso = tdPeso.textContent;
+for(let i = 0; i < pacientes.length; i++){
+    let paciente = pacientes[i];
 
-const tdAltura = paciente.querySelector(".info-altura");
-const altura = tdAltura.textContent;
-
-const tdImc = paciente.querySelector(".info-imc");
-
-const pesoValido = true;
-const alturaValida = true;
-
-if(peso <= 0 || peso >= 1000){
-    console.log("Peso inválido")
-    pesoValido = false;
-    tdImc.textContent = "Peso inválido!";
-}
-if(altura <= 0 || altura >= 3.00){
-    console.log("Altura inválida");
-    alturaValida = false;
-    tdImc.textContent = "Altura inválida!";
-}
-
-if(alturaValida && pesoValido){
-    const imc = peso / (altura * altura);
-    tdImc.textContent = imc
+    const tdPeso = paciente.querySelector(".info-peso");
+    const peso = tdPeso.textContent;
+    
+    const tdAltura = paciente.querySelector(".info-altura");
+    const altura = tdAltura.textContent;
+    
+    const tdImc = paciente.querySelector(".info-imc");
+    
+    const pesoValido = true;
+    const alturaValida = true;
+    
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido")
+        pesoValido = false;
+        tdImc.textContent = "Peso inválido!";
+    }
+    if(altura <= 0 || altura >= 3.00){
+        console.log("Altura inválida");
+        alturaValida = false;
+        tdImc.textContent = "Altura inválida!";
+    }
+    
+    if(alturaValida && pesoValido){
+        const imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
