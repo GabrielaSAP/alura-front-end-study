7e7f7136ec28638b416c70511b7cd1745e5c34bb
Gabriela Pestana <gabrielasap19@gmail.com>
2023-03-02 18:45:55 -0300

    adding patients to the table

--- aparecida-nutricao/js/principal.js
+++ aparecida-nutricao/js/principal.js
@@ -39,5 +39,37 @@ for(let i = 0; i < pacientes.length; i++){
 const botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click",function(event){
     event.preventDefault();
-    console.log("Oi, eu sou o botão e fui clicado!")
+    
+    const form = document.querySelector("#form-adiciona");
+
+    const nome = form.nome.value;
+    const peso = form.peso.value;
+    const altura = form.altura.value;
+    const gordura = form.gordura.value;
+
+    const pacienteTr = document.createElement("tr");
+
+    const nomeTd = document.createElement("td");
+    const pesoTd = document.createElement("td");
+    const alturaTd = document.createElement("td");
+    const gorduraTd = document.createElement("td");
+    const imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = peso / (altura * altura);
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd.toFixed(2));
+
+    const tabela = document.querySelector("#tabela-pacientes");
+    
+    tabela.appendChild(pacienteTr);
+
+    console.log(form);
 })
