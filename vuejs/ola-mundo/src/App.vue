674e91a30f6bed7968cf6670916ca46f1003dd84
Gabriela Pestana <gabrielasap19@gmail.com>
2023-05-04 13:22:23 -0300

    Preparando o ambiente com Vue

---
+++ vuejs/ola-mundo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <OlaMundo />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import OlaMundo from "@/components/OlaMundo.vue"
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    OlaMundo
+  }
+});
+</script>
---
+++ vuejs/ola-mundo/src/components/OlaMundo.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Olá mundo!</h1>
+    <h2>Primeiro projeto em VUE 3!!!!!!</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: "OlaMundo"
+})
+</script>
+
+<style>
+h1 {
+    color: blue;
+}
+</style>
