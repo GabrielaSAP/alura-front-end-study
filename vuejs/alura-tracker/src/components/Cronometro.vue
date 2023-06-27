0e09dd1cc372f18375142e7591096420f99a2758
Gabriela Pestana <gabrielasap19@gmail.com>
2023-06-27 15:00:07 -0300

    Envolving the cronometer

---
+++ vuejs/alura-tracker/src/components/Cronometro.vue
@@ -0,0 +1,25 @@
+<template>
+    <section>
+        <strong>{{ tempoDecorrido }}</strong>
+    </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Cronometro',
+    props: {
+        tempoEmSegundos: {
+            type: Number,
+            default: 0 
+        }
+    },
+    computed: {
+        tempoDecorrido() : string {
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
+        }
+    },
+})
+</script>
--- vuejs/alura-tracker/src/components/Formulario.vue
+++ vuejs/alura-tracker/src/components/Formulario.vue
@@ -6,9 +6,7 @@
             </div>
             <div class="column">
                 <div class="is-flex is-align-items-center is-justify-content-space-between">
-                    <section>
-                        <strong>{{ tempoDecorrido }}</strong>
-                    </section>
+                    <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
                     <button class="button" @click="iniciar">
                         <span class="icon">
                             <i class="fas fa-play"></i>
@@ -29,19 +27,17 @@
 
 <script lang="ts">
     import { defineComponent } from 'vue';
+    import Cronometro from './Cronometro.vue'
 
     export default defineComponent({
         name: 'FormularioPrincipal',
-
+        components: {
+            Cronometro
+        },
         data() {
             return {
                 tempoEmSegundos: 0,
                 cronometro: 0
-            }
-        },
-        computed: {
-            tempoDecorrido() : string {
-                return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11,8)
             }
         },
         methods: {
