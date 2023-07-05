4c4bcfb2ad164390c8620e8bf459f40cf6529464
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-05 09:55:44 -0300

    Negociação, modelagem e regras

--- typescript/typescript-curso-1/dist/js/app.js
+++ typescript/typescript-curso-1/dist/js/app.js
@@ -1,1 +1,6 @@
-alert('Oi')
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+console.log(negociacao);
+negociacao.quantidade = 10000;
+console.log(negociacao);
