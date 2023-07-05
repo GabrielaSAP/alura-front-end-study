f0c9d4b3e7f47e4a217b06e7a7b67a228bfed7c5
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-05 09:14:23 -0300

    Um estado para a todos governar

---
+++ vuejs/alura-tracker/src/store/index.ts
@@ -0,0 +1,28 @@
+import IProjeto from "@/interfaces/IProjeto";
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
+
+interface Estado {
+    projetos: IProjeto[]
+}
+
+export const key: InjectionKey<Store<Estado>> = Symbol()
+
+export const store = createStore<Estado>({
+    state: {
+        projetos: [
+            {
+                id: new Date().toISOString(),
+                nome: 'TypeScript'
+            },
+            {
+                id: new Date().toISOString(),
+                nome: 'Vue'
+            },
+            {
+                id: new Date().toISOString(),
+                nome: 'Vuex'
+            }
+        ]
+    }
+})
