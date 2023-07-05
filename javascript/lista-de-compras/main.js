26cdaa173082fa374c49527c52f579fb053d6433
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-05 13:52:19 -0300

    Clonando objetos

--- javascript/lista-de-compras/main.js
+++ javascript/lista-de-compras/main.js
@@ -1,3 +1,19 @@
-const listaDeItens = [
-    
-]
+let listaDeItens = []
+
+const form = document.getElementById('form-itens')
+const itensInput = document.getElementById('receber-item')
+
+form.addEventListener('submit', function (evento) {
+    evento.preventDefault()
+    salvarItem()
+})
+
+function salvarItem() {
+    const comprasItem = itensInput.value
+
+    listaDeItens.push({
+        valor: comprasItem
+    }) 
+
+    console.log(listaDeItens)
+}
