737a5862b8e130900eed975559738fb9a958f290
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-06 13:51:31 -0300

    Manipulando objetos no DOM

--- javascript/lista-de-compras/main.js
+++ javascript/lista-de-compras/main.js
@@ -3,11 +3,13 @@ let listaDeItens = []
 const form = document.getElementById('form-itens')
 const itensInput = document.getElementById('receber-item')
 const ulItens = document.getElementById('lista-de-itens')
+const ulItensComprados = document.getElementById('itens-comprados')
 
 form.addEventListener('submit', function (evento) {
     evento.preventDefault()
     salvarItem()
     mostrarItem()
+    itensInput.focus()
 })
 
 function salvarItem() {
@@ -23,24 +25,39 @@ function salvarItem() {
         }) 
     }
 
-    console.log(listaDeItens)
+    itensInput.value = ''
 }
 
 function mostrarItem() {
     ulItens.innerHTML = ''
+    ulItensComprados.innerHTML = ''
 
     listaDeItens.forEach((elemento, index) => {
-        ulItens.innerHTML += `
-        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
-            <div>
-                <input type="checkbox" class="is-clickable" />
-                <input type="text" class="is-size-5" value="${elemento.valor}"></input>
-            </div>
-            <div>
-                <i class="fa-solid fa-trash is-clickable deletar"></i>
-            </div>
-        </li>
-        `
+        if(elemento.checar) {
+            ulItensComprados.innerHTML += `
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" checked class="is-clickable" />  
+                    <span class="itens-comprados is-size-5">${elemento.valor}</span>
+                </div>
+                <div>
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+            </li>
+            `
+        } else {
+            ulItens.innerHTML += `
+            <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                <div>
+                    <input type="checkbox" class="is-clickable" />
+                    <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+                </div>
+                <div>
+                    <i class="fa-solid fa-trash is-clickable deletar"></i>
+                </div>
+            </li>
+            `
+        }
     })
 
     const inputsCheck = document.querySelectorAll('input[type="checkbox"]')
@@ -49,7 +66,7 @@ function mostrarItem() {
         i.addEventListener('click', (evento) => {
             const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens[valorDoElemento].checar = evento.target.checked
-            console.log(listaDeItens[valorDoElemento].checar)
+            mostrarItem()
         })
     })
 }
