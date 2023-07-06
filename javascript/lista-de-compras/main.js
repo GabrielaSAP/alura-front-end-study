2b8b13f4f45de0917ec04f5564b2b7ce331ba9e4
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-06 14:11:27 -0300

    Identificando índice de objetos

--- javascript/lista-de-compras/main.js
+++ javascript/lista-de-compras/main.js
@@ -1,4 +1,5 @@
 let listaDeItens = []
+let itemAEditar 
 
 const form = document.getElementById('form-itens')
 const itensInput = document.getElementById('receber-item')
@@ -53,6 +54,7 @@ function mostrarItem() {
                     <input type="text" class="is-size-5" value="${elemento.valor}"></input>
                 </div>
                 <div>
+                    <i class="fa-regular fa-floppy-disk is-clickable"></i><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
                     <i class="fa-solid fa-trash is-clickable deletar"></i>
                 </div>
             </li>
@@ -64,7 +66,7 @@ function mostrarItem() {
 
     inputsCheck.forEach(i => {
         i.addEventListener('click', (evento) => {
-            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens[valorDoElemento].checar = evento.target.checked
             mostrarItem()
         })
@@ -74,9 +76,19 @@ function mostrarItem() {
 
     deletarObjetos.forEach(i => {
         i.addEventListener('click', (evento) => {
-            const valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
+            valorDoElemento = evento.target.parentElement.parentElement.getAttribute('data-value')
             listaDeItens.splice(valorDoElemento,1)
             mostrarItem()
+        })
+    })
+
+    const editarItens = document.querySelectorAll('.editar')
+
+    editarItens.forEach(i => {
+        i.addEventListener('click', (evento) => {
+            itemAEditar = evento.target.parentElement.parentElement.getAttribute('data-value')
+            mostrarItem()
+            console.log(itemAEditar)
         })
     })
 }
