2c23a2462d0e510a1ea8837325ab005370507466
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-10 14:45:07 -0300

    GetItem

--- javascript/lista-de-compras/main.js
+++ javascript/lista-de-compras/main.js
@@ -5,9 +5,17 @@ const form = document.getElementById('form-itens')
 const itensInput = document.getElementById('receber-item')
 const ulItens = document.getElementById('lista-de-itens')
 const ulItensComprados = document.getElementById('itens-comprados')
+const listaRecuperada = localStorage.getItem('listaDeItens')
 
 function atualizaLocalStorage() {
     localStorage.setItem('listaDeItens', JSON.stringify(listaDeItens))
+}
+
+if(listaRecuperada) {
+    listaDeItens = JSON.parse(listaRecuperada)
+    mostrarItem()
+} else {
+    listaDeItens = []
 }
 
 form.addEventListener('submit', function (evento) {
