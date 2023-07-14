2bfb663606d889af9e52146cfaa75d010318d2b2
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-14 10:16:07 -0300

    Desafio: Classe Mago

--- javascript/devs-e-dragons/src/index.js
+++ javascript/devs-e-dragons/src/index.js
@@ -1,5 +1,6 @@
 import { Personagem } from "./modules/personagem.js"
 import { PersonagemView } from "./components/personagem-view.js"
+import { Mago } from "./modules/mago.js"
 
 const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago')
 const personagemJose = new Personagem('José', 3, 'Arqueiro')
---
+++ javascript/devs-e-dragons/src/modules/mago.js
@@ -0,0 +1,11 @@
+export class Mago {
+    elementoMagico
+    nivelMagico
+    inteligencia
+
+    constructor(elementoMagico, nivelMagico, inteligencia) {
+        this.elementoMagico = elementoMagico
+        this.nivelMagico = nivelMagico
+        this.inteligencia = inteligencia
+    }
+}
