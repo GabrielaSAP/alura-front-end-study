6413d7b2791c079aef894677ae7c934f48e55572
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-18 13:25:03 -0300

    Métodos estáticos

--- javascript/devs-e-dragons/src/modules/personagem.js
+++ javascript/devs-e-dragons/src/modules/personagem.js
@@ -17,4 +17,14 @@ export class Personagem {
         }
         return `${this.constructor.tipo} iniciante`
     }
+
+    static verificarVencedor(personagem1, personagem2) {
+        if(personagem1.nivel === personagem2.nivel) {
+            return 'Empate!!!'
+        }
+        if(personagem1.nivel > personagem2.nivel) {
+            return `${personagem1.constructor.tipo} ${personagem1.nome} é o vencedor!`
+        }
+        return `${personagem2.constructor.tipo} ${personagem2.nome} é o vencedor!`
+    }
 }
