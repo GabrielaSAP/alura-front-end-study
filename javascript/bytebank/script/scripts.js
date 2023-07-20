994501bdd902c4b2d419d5fa897d70730e6566e7
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-20 13:05:35 -0300

    Event Loop

--- javascript/bytebank/script/scripts.js
+++ javascript/bytebank/script/scripts.js
@@ -12,10 +12,10 @@ const graficoParaDolar = new Chart(graficoDolar, {
     }
 });
 
+setInterval(() => conectaAPI(), 5000)
+
 async function conectaAPI() {
     const conecta = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
     const conectaTraduzido = await conecta.json();
     console.log(conectaTraduzido);
 }
-
-setInterval(() => conectaAPI(), 5000)
