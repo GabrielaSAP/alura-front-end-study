a512ea2a43946cc7027799e55576de49921b9d5a
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-20 12:54:41 -0300

    FetchAPI

--- javascript/bytebank/script/scripts.js
+++ javascript/bytebank/script/scripts.js
@@ -10,4 +10,12 @@ const graficoParaDolar = new Chart(graficoDolar, {
             borderWidth: 1,
         }]
     }
-})
+});
+
+async function conectaAPI() {
+    const conecta = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
+    const conectaTraduzido = await conecta.json();
+    console.log(conectaTraduzido);
+}
+
+conectaAPI();
