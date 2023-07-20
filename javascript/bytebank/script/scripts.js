d9175b422e438337f1039b4086114272763af75e
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-20 12:47:23 -0300

    Script.js

---
+++ javascript/bytebank/script/scripts.js
@@ -0,0 +1,13 @@
+const graficoDolar = document.getElementById('graficoDolar')
+
+const graficoParaDolar = new Chart(graficoDolar, {
+    type: 'line',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+        }]
+    }
+})
