f9a385d6acb0c8b710a97f7dff57faaac1014e97
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-20 13:17:16 -0300

    Dados dinâmicos

--- javascript/bytebank/script/scripts.js
+++ javascript/bytebank/script/scripts.js
@@ -3,21 +3,21 @@ const graficoDolar = document.getElementById('graficoDolar')
 const graficoParaDolar = new Chart(graficoDolar, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Dólar',
+            data: [],
             borderWidth: 1,
         }]
     }
 });
-
 setInterval(() => conectaAPI(), 5000)
-
 async function conectaAPI() {
     const conecta = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
     const conectaTraduzido = await conecta.json();
-    console.log(conectaTraduzido);
+    let tempo = geraHorario();
+    let valor = conectaTraduzido.USDBRL.ask;
+    adicionarDados(graficoParaDolar, tempo, valor)
 }
 
 function geraHorario() {
@@ -27,4 +27,10 @@ function geraHorario() {
     return horario;
 }
 
-geraHorario();
+function adicionarDados(grafico, legenda, dados) {
+    grafico.data.labels.push(legenda);
+    grafico.data.datasets.forEach((dataset) => {
+        dataset.data.push(dados)
+    })
+    grafico.update();
+}
