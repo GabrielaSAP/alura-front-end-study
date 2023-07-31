ba6592b027590c50c9c00640e92df52c1105e0b7
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-31 16:47:38 -0300

    Mensagem view

--- typescript/typescript-curso-1/app/controllers/negociacao-controller.ts
+++ typescript/typescript-curso-1/app/controllers/negociacao-controller.ts
@@ -1,6 +1,7 @@
 import { NegociacoesView } from './../views/negociacoes-view.js';
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { MensagemView } from '../views/mensagem-view.js';
 
 export class NegociacaoController {
     private inputData: HTMLInputElement;
@@ -8,6 +9,7 @@ export class NegociacaoController {
     private inputValor: HTMLInputElement;
     private negociacoes = new Negociacoes();
     private negociacoesView = new NegociacoesView('#negociacoesView');
+    private mensagemView = new MensagemView('#mensagemView');
 
     constructor() {
         this.inputData = document.querySelector('#data');
@@ -20,6 +22,7 @@ export class NegociacaoController {
         const negociacao = this.criaNegociacao();
         this.negociacoes.adiciona(negociacao);
         this.negociacoesView.update(this.negociacoes);
+        this.mensagemView.update('Negociação adicionada com sucesso');
         this.limparFormulario();
     }
     
