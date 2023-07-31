54d9df8bb977c1a95b64028b1bf70b4c92261eb9
Gabriela Pestana <gabrielasap19@gmail.com>
2023-07-31 16:39:00 -0300

    Formatando a data

--- typescript/typescript-curso-1/app/views/negociacoes-view.ts
+++ typescript/typescript-curso-1/app/views/negociacoes-view.ts
@@ -22,7 +22,7 @@ export class NegociacoesView {
             ${model.lista().map(negociacao => {
                 return `
                 <tr>
-                    <td>?</td>
+                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
                     <td>${negociacao.quantidade}</td>
                     <td>${negociacao.valor}</td>
                 </tr>
--- typescript/typescript-curso-1/dist/js/views/negociacoes-view.js
+++ typescript/typescript-curso-1/dist/js/views/negociacoes-view.js
@@ -16,7 +16,7 @@ export class NegociacoesView {
             ${model.lista().map(negociacao => {
             return `
                 <tr>
-                    <td>?</td>
+                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
                     <td>${negociacao.quantidade}</td>
                     <td>${negociacao.valor}</td>
                 </tr>
