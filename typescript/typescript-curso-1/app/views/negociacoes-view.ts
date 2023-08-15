522cc6af97a738827b5522ccc1ab0d4b81334e7f
Gabriela Pestana <gabrielasap19@gmail.com>
2023-08-15 15:01:30 -0300

    Métodos privados

--- typescript/typescript-curso-1/app/views/negociacoes-view.ts
+++ typescript/typescript-curso-1/app/views/negociacoes-view.ts
@@ -17,7 +17,7 @@ export class NegociacoesView extends View<Negociacoes> {
             ${model.lista().map(negociacao => {
                 return `
                 <tr>
-                    <td>${new Intl.DateTimeFormat().format(negociacao.data)}</td>
+                    <td>${this.formatar(negociacao.data)}</td>
                     <td>${negociacao.quantidade}</td>
                     <td>${negociacao.valor}</td>
                 </tr>
@@ -26,5 +26,9 @@ export class NegociacoesView extends View<Negociacoes> {
             </tbody>
         </table>
         `;
+    }
+
+    private formatar(data: Date): string {
+        return new Intl.DateTimeFormat().format(data)
     }
 }
