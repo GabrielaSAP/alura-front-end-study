4e13fe2fd3151cb2e56f5063051a33eb2869c34c
Gabriela Pestana <gabrielasap19@gmail.com>
2023-08-16 18:45:12 -0300

    Isolando a lógica de conversão de entrada

--- typescript/typescript-curso-1/app/controllers/negociacao-controller.ts
+++ typescript/typescript-curso-1/app/controllers/negociacao-controller.ts
@@ -20,7 +20,12 @@ export class NegociacaoController {
     }
 
     public adiciona(): void {
-        const negociacao = this.criaNegociacao();
+        const negociacaoTemp = new Negociacao(null, 0, 0);
+        const negociacao = negociacaoTemp.criaDe(
+            this.inputData.value,
+            this.inputQuantidade.value,
+            this.inputValor.value
+        );
         if (!this.ehDiaUtil(negociacao.data)) {
             this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
             return;
@@ -34,14 +39,6 @@ export class NegociacaoController {
     private ehDiaUtil(data: Date) {
         return data.getDay() > DiasDaSemana.DOMINGO && 
                data.getDay() < DiasDaSemana.SABADO;
-    }
-    
-    private criaNegociacao(): Negociacao {
-        const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
-        const quantidade = parseInt(this.inputQuantidade.value);
-        const valor = parseFloat(this.inputValor.value);
-        return new Negociacao(date, quantidade, valor);
     }
 
     private limparFormulario(): void {
