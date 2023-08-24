6e409bb6b3b278abe3730571154848af71d6752a
Gabriela Pestana <gabrielasap19@gmail.com>
2023-08-24 14:39:11 -0300

    Recorrendo ao JavaScript

--- typescript/typescript-curso-3/app/src/decorators/logar-tempo-de-execucao.ts
+++ typescript/typescript-curso-3/app/src/decorators/logar-tempo-de-execucao.ts
@@ -5,9 +5,9 @@ export function logarTempoDeExecucao() {
         descriptor: PropertyDescriptor
     ) {
         const metodoOriginal = descriptor.value;
-        descriptor.value = function() {
+        descriptor.value = function(...args: any[]) {
             const t1 = performance.now();
-            const retorno = metodoOriginal();
+            const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
             console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/1000} segundos`)
             retorno
