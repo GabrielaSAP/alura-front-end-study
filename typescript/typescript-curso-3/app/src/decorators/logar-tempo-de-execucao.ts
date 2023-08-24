fb6e8e7dbd0531e5b95c84517c7c2ff2f5a65533
Gabriela Pestana <gabrielasap19@gmail.com>
2023-08-24 14:27:22 -0300

    Implementando nosso Decorator

--- typescript/typescript-curso-3/app/src/decorators/logar-tempo-de-execucao.ts
+++ typescript/typescript-curso-3/app/src/decorators/logar-tempo-de-execucao.ts
@@ -4,6 +4,15 @@ export function logarTempoDeExecucao() {
         propertyKey: string,
         descriptor: PropertyDescriptor
     ) {
-        return descriptor
+        const metodoOriginal = descriptor.value;
+        descriptor.value = function() {
+            const t1 = performance.now();
+            const retorno = metodoOriginal();
+            const t2 = performance.now();
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1)/1000} segundos`)
+            retorno
+        };
+
+        return descriptor;
     }
 }
