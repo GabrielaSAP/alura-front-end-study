1aceafbbbd414386964eabc929feddf207e6ec40
Gabriela Pestana <gabrielasap19@gmail.com>
2023-08-28 17:24:50 -0300

    Criando um Decorator de inspeção

--- typescript/typescript-curso-3/app/src/views/view.ts
+++ typescript/typescript-curso-3/app/src/views/view.ts
@@ -1,3 +1,4 @@
+import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 
 export abstract class View<T> {
@@ -16,7 +17,7 @@ export abstract class View<T> {
             this.escapar = escapar;
         }
     }
-
+    @inspect()
     @logarTempoDeExecucao(true)
     public update(model: T): void {
         let template = this.template(model);
