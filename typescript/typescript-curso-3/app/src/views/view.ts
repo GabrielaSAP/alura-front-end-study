c1793c06107893af0d9c1f2e3096c66a2fe73cbb
Gabriela Pestana <gabrielasap19@gmail.com>
2023-08-28 17:29:45 -0300

    Ordem de execução dos Decorators

--- typescript/typescript-curso-3/app/src/views/view.ts
+++ typescript/typescript-curso-3/app/src/views/view.ts
@@ -17,8 +17,8 @@ export abstract class View<T> {
             this.escapar = escapar;
         }
     }
-    @inspect()
     @logarTempoDeExecucao(true)
+    @inspect()
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
