80b68445b3639ee3439695192d5e1a98d14c3adc
Gabriela Pestana <gabrielasap19@gmail.com>
2023-08-31 11:23:33 -0300

    Criando getters dinamicamente

--- typescript/typescript-curso-3/app/dist/js/decorators/dom-injector.js
+++ typescript/typescript-curso-3/app/dist/js/decorators/dom-injector.js
@@ -1,4 +1,8 @@
 export function domInjector(seletor) {
     return function (target, propertyKey) {
+        const getter = function () {
+            const elemento = document.querySelector(seletor);
+            return elemento;
+        };
     };
 }
--- typescript/typescript-curso-3/app/src/decorators/dom-injector.ts
+++ typescript/typescript-curso-3/app/src/decorators/dom-injector.ts
@@ -1,5 +1,9 @@
 export function domInjector(seletor: string) {
     return function(target: any, propertyKey: string) {
         
+        const getter = function() {
+            const elemento = document.querySelector(seletor);
+            return elemento;
+        }   
     }
 }
