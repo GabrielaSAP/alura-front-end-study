c053ce15c16f4458e6152fae87e05663ed6dac63
Gabriela Pestana <gabrielasap19@gmail.com>
2023-09-06 17:55:50 -0300

    Transformando tarefas em mutações

--- vuejs/alura-tracker/src/store/index.ts
+++ vuejs/alura-tracker/src/store/index.ts
@@ -1,32 +1,52 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import { ADICIONA_PROJETO, ALTERA_PROJETO, EXCLUIR_PROJETO } from "./tipo-mutacoes";
+import { 
+    ADICIONA_PROJETO, 
+    ADICIONA_TAREFA, 
+    ALTERA_PROJETO, 
+    ATUALIZA_TAREFA, 
+    EXCLUIR_PROJETO, 
+    REMOVE_TAREFA } from "./tipo-mutacoes";
+import ITarefa from "@/interfaces/ITarefa";
 
 interface Estado {
-    projetos: IProjeto[]
+    projetos: IProjeto[],
+    tarefas: ITarefa[]
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
     state: {
-        projetos: []
+        projetos: [],
+        tarefas: []
     },
     mutations: {
-        [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
+        [ADICIONA_PROJETO] (state, nomeDoProjeto: string) {
             const projeto = {
                 id: new Date().toISOString(),
                 nome: nomeDoProjeto
             } as IProjeto
             state.projetos.push(projeto)
         },
-        [ALTERA_PROJETO](state, projeto: IProjeto) {
+        [ALTERA_PROJETO] (state, projeto: IProjeto) {
             const index = state.projetos.findIndex(proj => proj.id == projeto.id)
             state.projetos[index] = projeto
         },
-        [EXCLUIR_PROJETO](state, id: string) {
+        [EXCLUIR_PROJETO] (state, id: string) {
             state.projetos = state.projetos.filter(proj => proj.id != id)
+        },
+        [ADICIONA_TAREFA] (state, tarefa: ITarefa) {
+            tarefa.id = new Date().toISOString()
+            state.tarefas.push(tarefa)
+        },
+        [ATUALIZA_TAREFA] (state, tarefa: ITarefa) {
+            const indice = state.tarefas.findIndex(p => p.id == tarefa.id)
+            state.tarefas[indice] = tarefa
+        },
+        [REMOVE_TAREFA] (state, id: string) {
+            state.tarefas = state.tarefas.filter(p => p.id != id)
         }
     }
 })
