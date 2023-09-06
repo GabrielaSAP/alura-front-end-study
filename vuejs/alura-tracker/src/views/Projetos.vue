f04128623610f968a17dd8d3743dc507894c8c65
Gabriela Pestana <gabrielasap19@gmail.com>
2023-09-06 17:20:34 -0300

    Rotas aninhadas

--- vuejs/alura-tracker/src/views/Projetos.vue
+++ vuejs/alura-tracker/src/views/Projetos.vue
@@ -1,52 +1,18 @@
 <template>
-    <section class="projetos">
+    <div class="projetos">
         <h1 class="title">Projetos</h1>
-        <RouterLink to="/projetos/novo" class="button">
-            <span class="icon is-small">
-                <i class="fas fa-plus"></i>
-            </span>
-            <span>Novo projeto</span>
-        </RouterLink>
-        <table class="table is-fullwidth">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="projeto in projetos" :key="projeto.id">
-                    <td>{{ projeto.id }}</td>
-                    <td>{{ projeto.nome }}</td>
-                    <td>
-                        <RouterLink :to="`/projetos/${projeto.id}`" class="button">
-                            <span class="icon is-small">
-                                <i class="fas fa-pencil-alt"></i>
-                            </span>
-                        </RouterLink>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </section>
+        <routerView></routerView>
+    </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue';
-import { useStore } from '@/store';
+    import { defineComponent } from 'vue';
 
-export default defineComponent({
-    // eslint-disable-next-line vue/multi-word-component-names
-    name: 'Projetos',
-    setup() {
-        const store = useStore()
-        return {
-            projetos: computed(() => store.state.projetos)
-        }
-    }
-});
-</script> 
+    export default defineComponent({
+        // eslint-disable-next-line vue/multi-word-component-names
+        name: 'Projetos'
+    });
+</script>
 
 <style scoped>
     .projetos {
---
+++ vuejs/alura-tracker/src/views/Projetos/Lista.vue
@@ -0,0 +1,48 @@
+<template>
+    <section>
+        <RouterLink to="/projetos/novo" class="button">
+            <span class="icon is-small">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span>Novo projeto</span>
+        </RouterLink>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="projeto in projetos" :key="projeto.id">
+                    <td>{{ projeto.id }}</td>
+                    <td>{{ projeto.nome }}</td>
+                    <td>
+                        <RouterLink :to="`/projetos/${projeto.id}`" class="button">
+                            <span class="icon is-small">
+                                <i class="fas fa-pencil-alt"></i>
+                            </span>
+                        </RouterLink>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+import { useStore } from '@/store';
+
+export default defineComponent({
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Lista',
+    setup() {
+        const store = useStore()
+        return {
+            projetos: computed(() => store.state.projetos)
+        }
+    }
+});
+</script>
